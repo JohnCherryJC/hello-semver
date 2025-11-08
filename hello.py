b4831af41987473562cb8b8da7e7c3b87029260a
JohnCherryJC <artemromanenko378@gmail.com>
2025-11-08 16:36:33 +0300

    Initial commit: добавлен базовый Hello World

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hello.py
+import sys
+
+def greet(name="World"):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = sys.argv[1] if len(sys.argv) > 1 else "World"
+    print(greet(name))
