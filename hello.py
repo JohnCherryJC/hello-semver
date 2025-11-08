6a5ffffe316ed21fdededb1ed9e13ff482209c58
JohnCherryJC <artemromanenko378@gmail.com>
2025-11-08 16:47:00 +0300

    Добавлена персонализированная функция приветствия

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 import sys
 
 def greet(name="World"):
-    return f"Hello, {name}!"
+    return f"Hello, {name}! Welcome to Semantic Versioning Demo."
 
 if __name__ == "__main__":
     name = sys.argv[1] if len(sys.argv) > 1 else "World"
